a94e0093bde38f7b708c396b3de3df32824b76af
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-01-22 04:00:37 -0300

    FEAT: Add flip images on manipulation

--- distortions/utils/files_manipulation.py
+++ distortions/utils/files_manipulation.py
@@ -7,6 +7,70 @@ class FilesManipulation:
     def __init__(self):
         pass
 
+    def flip_single_image(self, 
+                   image_path: str, 
+                   flip_type: str
+                ) -> Image.Image:
+        """
+        Função para flipar uma imagem.
+
+        Parâmetros:
+        - image_path: Caminho para a imagem a ser flipada.
+        - flip_type: Tipo de flip ('horizontal' ou 'vertical').
+
+        Retorna:
+        - A imagem flipada.
+        """
+        # Abre a imagem usando PIL
+        try:
+            img = Image.open(image_path)
+
+            # Verifica o tipo de flip e aplica
+            if flip_type == 'horizontal':
+                flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif flip_type == 'vertical':
+                flipped_img = img.transpose(Image.FLIP_TOP_BOTTOM)
+            elif flip_type == 'both':
+                flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
+            else:
+                raise ValueError("flip_type deve ser 'horizontal' ou 'vertical'")
+
+            return flipped_img
+        except Exception as e:
+            raise RuntimeError(f"Error to flip image {image_path}: {e}")
+
+    def flip_images(self, 
+                    directory: str, 
+                    types: List[str]
+                ) -> None:
+        main_directory = directory
+
+        print("🚀 Iniciando o script de flip de imagens...")
+
+        for folder_name in tqdm(os.listdir(main_directory)):
+            
+            folder_path = os.path.join(main_directory, folder_name)
+
+            if os.path.isdir(folder_path):
+                print(f"\n📁 Processando a pasta: {folder_name}")
+
+                for file_name in tqdm(os.listdir(folder_path)):
+                    
+                    file_path = os.path.join(folder_path, file_name)
+
+                    if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                        
+                        for flip_type in types:
+
+                            fliped_image = self.flip_single_image(file_path, flip_type=flip_type)
+
+                            if fliped_image:
+                                fliped_name = f"{flip_type}_{file_name}"
+                                
+                                fliped_path = os.path.join(folder_path, fliped_name)
+                                
+                                fliped_image.save(fliped_path)
+
     def crop_single_image(
             self, 
             input_file_path: str, 
