9c8f72f256bde748d02d07d5a3cdf14f658ffe8c
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-03-13 09:54:17 -0300

    UPDATES

--- test_yolo.py
+++ test_yolo.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from distortions.utils.bar_progress import BarProgress
+import cv2
 
 base_dir = "runs/test"
 os.makedirs(base_dir, exist_ok=True)
@@ -13,12 +14,13 @@ save_dir = f"{base_dir}/yolo_{count+1}"
 os.makedirs(save_dir, exist_ok=True)
 
 # Carregar modelo
-model = YOLO('runs/classify/train5/weights/best.pt')
+model_path = 'runs/classify/train9/weights/best.pt'
+model = YOLO(model_path)
 
 # Definições
 classes = {0: 'awgn', 1: 'blur', 2: 'contrast', 3: 'fnoise', 4: 'jpeg', 5: 'jpeg2000', 6: 'src'}
 class_names = [classes[i] for i in range(len(classes))] # [0, 1, 2, 3] -> nomes
-base_path = 'Datasets/CSIQ/train'
+base_path = 'Datasets/CSIQ_LAB/train'
 
 y_true = []
 y_pred = []
@@ -26,6 +28,8 @@ y_pred = []
 print("Iniciando inferência...")
 
 bar = BarProgress(header=['Current step', 'Total steps'], width=12)
+
+img_path = ""
 
 for key, value in classes.items():
     folder_path = os.path.join(base_path, value)
@@ -87,8 +91,23 @@ plt.xlabel('True Label')
 plt.ylabel('Predicted Label') 
 plt.savefig(f"{save_dir}/confusion_matrix_yolo.png", dpi=300)
 
+img = cv2.imread(img_path)
+hi, wi, _ = img.shape
+
 # calculo de acurácia
 accuracy = sum(1 for true, pred in zip(y_true, y_pred) if true == pred) / len(y_true) * 100
 print(f"Acurácia: {accuracy:.2f}%")
-with open(f"{save_dir}/accuracy.txt", "w") as f:
+with open(f"{save_dir}/informations.yaml", "w") as f:
     f.write(f"Acurácia: {accuracy:.2f}%\n")
+    f.write(f"Acertos: {sum(1 for true, pred in zip(y_true, y_pred) if true == pred)}\n")
+    f.write(f"Erros: {sum(1 for true, pred in zip(y_true, y_pred) if true != pred)}\n")
+    f.write(f"Total de amostras: {len(y_true)}\n")
+    f.write(f"Amostras por classe: { {class_names[i]: y_true.count(i) for i in range(len(class_names))} }\n")
+    f.write(f"Acertos por classe: { {class_names[i]: sum(1 for true, pred in zip(y_true, y_pred) if true == pred == i) for i in range(len(class_names))} }\n")
+    f.write(f"Erros por classe: { {class_names[i]: sum(1 for true, pred in zip(y_true, y_pred) if true == i and pred != i) for i in range(len(class_names))} }\n")
+    f.write(f"Dataset base: {base_path}\n")
+    f.write(f"Resolution: {wi}x{hi}\n")
+    f.write(f"Classes: {class_names}\n")
+    f.write(f"Modelo: {model_path}\n")
+
+
--- train_yolo.py
+++ train_yolo.py
@@ -5,14 +5,37 @@ def augment(image):
 
 model = YOLO('yolo26m-cls.pt') 
 
-results = model.train(
-    data='Datasets/Dist', 
+model.train(
+    data='Datasets/Dist_HSV', 
     epochs=30, 
     imgsz=512, 
     device=0, 
     batch=16,
-    save=True,        
-    save_period=1,    
+    # --- Desativando Augmentations ---
+    hsv_h=0.0,      # Ajuste de matiz (hue)
+    hsv_s=0.0,      # Ajuste de saturação
+    hsv_v=0.0,      # Ajuste de valor (brilho)
+    degrees=0.0,    # Rotação
+    translate=0.0,  # Translação
+    scale=0.0,      # Escala (ganho/perda de zoom)
+    shear=0.0,      # Cisalhamento
+    perspective=0.0,# Perspectiva
+    flipud=0.0,     # Inversão vertical
+    fliplr=0.5,     # Inversão horizontal 
+    mosaic=0.0,     # Desativa o mosaico (importante para detecção)
+    mixup=0.0,      # Desativa o MixUp (comum em classificação)
+    auto_augment=None, # Desativa políticas automáticas (como RandAugment)
+    erasing=0.0     # Desativa o Random Erasing
+)
+
+model2 = YOLO('yolo26m-cls.pt') 
+
+model2.train(
+    data='Datasets/Dist_LAB', 
+    epochs=30, 
+    imgsz=512, 
+    device=0, 
+    batch=16,
     # --- Desativando Augmentations ---
     hsv_h=0.0,      # Ajuste de matiz (hue)
     hsv_s=0.0,      # Ajuste de saturação
