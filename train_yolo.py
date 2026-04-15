0267590f0d5a8ee56316304c1bc2f25713acb3af
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-04-15 18:29:13 -0300

    fix yolo

--- train_yolo.py
+++ train_yolo.py
@@ -1,63 +1,4 @@
-import torch
-import kornia
-import types
-import math
 from ultralytics import YOLO
-
-def inject_lab(trainer_or_validator):
-    if getattr(trainer_or_validator, "lab_patch_applied", False):
-        return
-    
-    trainer_or_validator.lab_patch_applied = True
-
-    # Detect the correct method name based on the object type (Trainer vs Validator)
-    method_name = "preprocess_batch" if hasattr(trainer_or_validator, "preprocess_batch") else "preprocess"
-    original_preprocess = getattr(trainer_or_validator, method_name)
-
-    def custom_preprocess(self, batch):
-        batch = original_preprocess(batch)
-        
-        images_rgb = batch['img']
-        images_lab = kornia.color.rgb_to_lab(images_rgb)
-        
-        l_channel = images_lab[:, 0:1, :, :] / 100.0
-        a_channel = (images_lab[:, 1:2, :, :] + 128.0) / 255.0
-        b_channel = (images_lab[:, 2:3, :, :] + 128.0) / 255.0
-        
-        batch['img'] = torch.cat([l_channel, a_channel, b_channel], dim=1)
-        
-        return batch
-
-    # Apply the patch to the correct method
-    setattr(trainer_or_validator, method_name, types.MethodType(custom_preprocess, trainer_or_validator))
-
-def inject_hsv(trainer_or_validator):
-    if getattr(trainer_or_validator, "hsv_patch_applied", False):
-        return
-    
-    trainer_or_validator.hsv_patch_applied = True
-
-    # Detect the correct method name based on the object type (Trainer vs Validator)
-    method_name = "preprocess_batch" if hasattr(trainer_or_validator, "preprocess_batch") else "preprocess"
-    original_preprocess = getattr(trainer_or_validator, method_name)
-
-    def custom_preprocess(self, batch):
-        batch = original_preprocess(batch)
-        
-        images_rgb = batch['img']
-        images_hsv = kornia.color.rgb_to_hsv(images_rgb)
-        
-        # Normalization
-        h_channel = images_hsv[:, 0:1, :, :] / (2 * math.pi)
-        s_channel = images_hsv[:, 1:2, :, :]
-        v_channel = images_hsv[:, 2:3, :, :]
-        
-        batch['img'] = torch.cat([h_channel, s_channel, v_channel], dim=1)
-        
-        return batch
-
-    # Apply the patch to the correct method
-    setattr(trainer_or_validator, method_name, types.MethodType(custom_preprocess, trainer_or_validator))
 
 weights = [
     'yolo26n-cls.pt',
@@ -67,24 +8,18 @@ weights = [
     'yolo26x-cls.pt'
 ]
 
-colors_space = {
-    'lab': inject_lab,
-    'hsv': inject_hsv
-}
+colors_space = ['LAB', 'HSV']
 
-for key, inject_function in colors_space.items():
+for color in colors_space:
 
     for weight in weights:
 
         model = YOLO(weight)  
 
-        model.add_callback("on_train_batch_start", inject_function)
-        model.add_callback("on_val_batch_start", inject_function)
-
         model.train(
-            data='Datasets/LIST', 
+            data=f'Datasets/LIST_{color}', 
             task='classify',
-            project=f'{weight.split("-")[0]}_{key}',
+            project=f'{weight.split("-")[0]}_{color}',
             name=weight.split('-')[1].split('.')[0],
             epochs=30, 
             imgsz=512, 
