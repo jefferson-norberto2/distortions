395fa76133d2e42bd0ee5750c6a690d801e5ee7e
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-05-22 04:07:32 -0300

    WILD CLASS

--- classify.py
+++ classify.py
@@ -2,27 +2,27 @@ from distortions.model.custom_mobilenet import CustomMobileNet
 import torch
 import yaml
 import os
+import shutil
 from PIL import Image
 from torchvision import transforms
 from tqdm import tqdm
 
-model = CustomMobileNet(num_classes=7, pre_trained=False, backbone='mobilenet_v1')
+args_path = '/run/media/jmn/Removable Disk/runs/trained/mobilenet/V1/HSV/args.yaml'
+with open(args_path, 'r') as file:
+    data = yaml.safe_load(file)
+
+class_names = data['class_names']
+ids = {i: name for i, name in enumerate(class_names)}
+
+model = CustomMobileNet(num_classes=data['num_classes'], pre_trained=False, backbone='mobilenet_v1')
 model.load_state_dict(torch.load('/run/media/jmn/Removable Disk/runs/trained/mobilenet/V1/HSV/best.pt' , map_location='cpu', weights_only=True))
 model.eval()
 
 dirs = ['Macro', 'Dark', 'Indoor', 'Outdoor', 'NightVision']
 for d in dirs:
     root_dir = f'/run/media/jmn/Removable Disk/database/{d}'
-    args_path = '/run/media/jmn/Removable Disk/runs/trained/mobilenet/V1/HSV/args.yaml'
-    dataset_save_path = f'Datasets/Wild_v2/{d}'
-
+    dataset_save_path = f'Datasets/Wild_V9/{d}'
     os.makedirs(dataset_save_path, exist_ok=True)
-
-    with open(args_path, 'r') as file:
-        data = yaml.safe_load(file)
-
-    class_names = data['class_names']
-    ids = {i: name for i, name in enumerate(class_names)}
 
     for class_name in class_names:
         os.makedirs(os.path.join(dataset_save_path, class_name), exist_ok=True)
@@ -39,5 +39,5 @@ for d in dirs:
             predicted_class = ids[predicted.item()]
             
         save_path = os.path.join(dataset_save_path, predicted_class, img_name)
-        img.save(save_path, "JPEG", quality=100, subsampling=0)
+        shutil.copy(img_path, save_path)
     
